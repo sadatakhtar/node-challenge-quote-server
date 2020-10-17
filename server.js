f4932af33cbc3254b95de6259f3af5b82f17e108
sadatakhtar <sadat0akhtar@gmail.com>
2020-10-17 19:10:50 +0100

    completed level 1 tasks

--- server.js
+++ server.js
@@ -13,11 +13,18 @@ const quotes = require("./quotes.json");
 //   /quotes            - Should return all quotes (json)
 //   /quotes/random     - Should return ONE quote (json)
 app.get("/", function (request, response) {
-  response.send("Neill's Quote Server!  Ask me for /quotes/random, or /quotes");
+  response.send("CYF's Quote Server!  Ask me for /quotes/random, or /quotes");
 });
 
 //START OF YOUR CODE...
+app.get('/quotes', (req, res) => {
+  res.json(quotes);
+})
 
+app.get('/quotes/random', (req, res) => {
+ let randomNumber = [Math.floor(Math.random() * quotes.length)];
+ res.send(quotes[randomNumber]);
+ });
 //...END OF YOUR CODE
 
 //You can use this function to pick one element at random from a given array
@@ -29,6 +36,6 @@ function pickFromArray(arr) {
 }
 
 //Start our server so that it listens for HTTP requests!
-const listener = app.listen(process.env.PORT, function () {
-  console.log("Your app is listening on port " + listener.address().port);
+const listener = app.listen(9000, function () {
+  console.log("Your app is listening on port 9000");
 });
