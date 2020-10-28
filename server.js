fba4be9970cdee5ddf41ae2c40348de28822c2ec
sadatakhtar <sadat0akhtar@gmail.com>
2020-10-28 14:20:35 +0000

    changed server listening port

--- server.js
+++ server.js
@@ -56,6 +56,10 @@ function pickFromArray(arr) {
 }
 
 //Start our server so that it listens for HTTP requests!
-const listener = app.listen(9000, function () {
-  console.log("Your app is listening on port 9000");
+// const listener = app.listen(9000, function () {
+//   console.log("Your app is listening on port 9000");
+// });
+
+const listener = app.listen(process.env.PORT, function() {
+  console.log("Your app is listening on port " + listener.address().port);
 });
